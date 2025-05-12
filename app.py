2d3ef4b938d7e7a2c4c036741e98d5dbe2e65cf8
techatha <techatham_p@cmu.ac.th>
2025-05-12 18:03:07 +0700

    Add index.html file for collecting data

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(ssl_context='adhoc')
